05ae624ad007d690788bfed8218e7237210f8b73
Pedro Espin <pedroesping@gmail.com>
2022-10-12 01:18:20 +0200

    initial commit

---
+++ PrimitivosEnteros.java
@@ -0,0 +1,45 @@
+public class PrimitivosEnteros {
+    public static void main(String[] args) {
+
+        byte numeroBy = 127;
+        System.out.println("numeroBy = " + numeroBy);
+        System.out.println("tipo byte corresponde en byte a " + Byte.BYTES);
+        System.out.println("tipo byte corresponde en byte a " + Byte.SIZE);
+        System.out.println("valor maximo de un byte: " + Byte.MAX_VALUE);
+        System.out.println("valor minimo de un byte: " + Byte.MIN_VALUE);
+
+        Short numeroShort = 32767;
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("tipo short corresponde en byte a " + Short.BYTES);
+        System.out.println("tipo short corresponde en byte a " + Short.SIZE);
+        System.out.println("valor maximo de un short: " + Short.MAX_VALUE);
+        System.out.println("valor minimo de un short: " + Short.MIN_VALUE);
+
+        int numeroInt = 2147483647;
+        System.out.println("numeroInt = " + numeroInt);
+
+        System.out.println("tipo int corresponde en byte a " + Integer.BYTES);
+        System.out.println("tipo int corresponde en byte a " + Integer.SIZE);
+        System.out.println("valor maximo de un int: " + Integer.MAX_VALUE);
+        System.out.println("valor minimo de un int: " + Integer.MIN_VALUE);
+        
+        long numeroLong = 9223372036854775807L;
+        System.out.println("numeroLong = " + numeroLong);
+
+        System.out.println("tipo long corresponde en byte a " + Long.BYTES);
+        System.out.println("tipo long corresponde en byte a " + Long.SIZE);
+        System.out.println("valor maximo de un long: " + Long.MAX_VALUE);
+        System.out.println("valor minimo de un long: " + Long.MIN_VALUE);
+
+        double numeroFloat = 3.4028235E38;
+        System.out.println("numeroFloat = " + numeroFloat);
+
+        System.out.println("tipo float corresponde en byte a " + Float.BYTES);
+        System.out.println("tipo float corresponde en byte a " + Float.SIZE);
+        System.out.println("valor maximo de un float: " + Float.MAX_VALUE);
+        System.out.println("valor minimo de un float: " + Float.MIN_VALUE);
+
+        var numeroVar = 9223372036854775807f;
+
+    }
+}
---
+++ SistemasNumericos.java
@@ -0,0 +1,40 @@
+import javax.lang.model.SourceVersion;
+import javax.swing.*;
+public class SistemasNumericos {
+    public static void main(String[] args) {
+
+        String numeroStr = JOptionPane.showInputDialog(null,"Ingrese un número entero");
+        int numeroDecimal = 0;
+        try{
+            numeroDecimal = Integer.parseInt(numeroStr);
+        }catch (NumberFormatException e){
+            JOptionPane.showInputDialog(null,"Error debe ingresar número entero");
+            main(args);
+            System.exit(0);
+        }
+
+        System.out.println("numeroDecimal = " + numeroDecimal);
+
+        String resultadoBinario = "numero binario de = " + numeroDecimal + " = "+ Integer.toBinaryString(numeroDecimal);
+        System.out.println(resultadoBinario);
+
+        int numeroBinario = 0b11110;
+        System.out.println("numeroBinario = " + numeroBinario);
+
+        String resultadoOctal = "numero octal de " + numeroDecimal + " = " + Integer.toOctalString(numeroDecimal);
+        System.out.println( resultadoOctal);
+        int numeroOctal = 0764;
+        System.out.println("numeroOctal = " + numeroOctal);
+
+        String resultadoHex = "numero hexadecimal de = " + numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+        System.out.println(resultadoHex);
+        int numeroHex = 0x1f4;
+        System.out.println("numeroHex = " + numeroHex);
+
+        String mensaje = resultadoBinario;
+        mensaje += "\n" + resultadoOctal;
+        mensaje +="\n" + resultadoHex;
+
+        JOptionPane.showInputDialog(null,mensaje);
+    }
+}
